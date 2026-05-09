447296ab250daf6aa5c04d2b7aae3a63465e4c6d
Yuhang Zhang <yuhangz@bu.edu>
2026-05-09 19:27:40 -0400

    spring boot set up

---
+++ src/main/java/com/example/dailyquest/DailyquestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailyquest/DailyquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
